0a9cf76a7cfda7a38a83944f1d16453a39ca9cc2
gdutchak <galyna.dutchak99@gmail.com>
2022-09-24 17:23:11 +0300

    update prop

--- src/components/ListContacts/ListContacts.jsx
+++ src/components/ListContacts/ListContacts.jsx
@@ -10,7 +10,7 @@ export const ListContacts = ({ onSearch, onDelete }) => (
 
 
 ListContacts.propTypes = {
-    onSearch: PropTypes.arrayOf(PropTypes.string({
+    onSearch: PropTypes.arrayOf(PropTypes.shape({
         name: PropTypes.string.isRequired,
         number: PropTypes.string.isRequired,
         id: PropTypes.string.isRequired,
